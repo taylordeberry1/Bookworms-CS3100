b852ce9d8835f120ca7cf1adff0a44226087b306
taylordeberry1 <taylordeberry1@gmail.com>
2026-09-10 11:19:33 -0700

    Implement BookManager to manage book entries
    Removed introductory print statements and added book management functionality.

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My name is Taylor.");
-        System.out.println("I am learning Java.");
+
+        BookManager bookManager = new BookManager();
+
+        bookManager.addBook(new Book (
+            "The School for Good and Evil",
+            "Soman Chainani",
+            "Fantasy",
+            3.99
+        ));
+
+        bookManager.addBook (new Book (
+            "Harry Potter and the Sorcerer's Stone",
+            "J.K. Rowling",
+            "Fantasy",
+            4.5
+        ));
+
+        bookManager.printBooks();
     }
 }
+
