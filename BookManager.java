cd4015fbfce4cf6d851420c37143467bcc9dfe54
taylordeberry1 <taylordeberry1@gmail.com>
2026-09-10 11:20:02 -0700

    Implement BookManager class with add and print methods
    Added methods to manage and display a list of books.

--- BookManager.java
+++ BookManager.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class BookManager {
-    
+
+    private ArrayList<Book> books;
+
+    public BookManager() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void printBooks() {
+        for (Book book : books) {
+            System.out.println(
+                "Title: " + book.getTitle()
+                + "\nAuthor: " + book.getAuthor()
+                + "\nGenre: " + book.getGenre()
+                + "\nAverage Rating: " + book.getAverageRating()
+                + "\n"
+            );
+        }
+    }
 }
