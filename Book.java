eccc0880f561b278b8ee97b94957e05334a887d7
taylordeberry1 <taylordeberry1@gmail.com>
2026-09-10 11:20:29 -0700

    Add Book class with attributes and getters

--- Book.java
+++ Book.java
@@ -1,3 +1,28 @@
 public class Book {
-    
-}
+    private String title;
+    private String author;
+    private String genre;
+    private double averageRating;
+
+    public Book(String title, String author, String genre, double averageRating){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.averageRating = averageRating;
+    }
+     public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+    }
