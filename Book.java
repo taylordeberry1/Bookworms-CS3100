cb28b2033b7192ab064d357510409ba1feaa48d9
taylordeberry1 <taylordeberry1@gmail.com>
2026-09-24 09:38:58 -0700

    Enhance Book class with review management
    Added reviewList attribute and methods to manage reviews.

--- Book.java
+++ Book.java
@@ -1,68 +1,81 @@
 //Taylor DeBerry and Xandra Quevedo
 //Book object class containing constructors, getters, setters, and toString methods
 
+
+
+
 public class Book {
-    //Basic book attributes
-    private String title;
-    private String author;
-    private String genre;
-    private double averageRating;
+	//Basic book attributes
+	private String title;
+	private String author;
+	private String genre;
+	private double averageRating;
+	private ReviewList reviewList;
+	
+	//Parameterized constructor
+	public Book(String title, String author, String genre, double averageRating,
+			ReviewList reviewList
+			){
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.averageRating = averageRating; 
+		this.reviewList = reviewList;
+	}
 
-    //Parameterized constructor
-    public Book(String title, String author, String genre, double averageRating){
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.averageRating = averageRating; 
-        /*
-        A note for future implementation stuff:
-        This is the initial book constructor, so this will be used
-        in tandem with the file that has all the books, I think.
-        That'll be a method in Main most likely (pulling all
-        the books from the file, creating them, and adding them
-        to a list). -XQ
-        */
-    }
+	//Getters
+	public String getTitle() {
+		return title;
+	}
 
-    //Getters
-    public String getTitle() {
-        return title;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
+	public String getGenre() {
+		return genre;
+	}
 
-    public String getGenre() {
-        return genre;
-    }
+	public double getAverageRating() {
+		return averageRating;
+	}
+ 
+	public ReviewList getReviewList() {
+		return reviewList;
+	}
 
-    public double getAverageRating() {
-        return averageRating;
-    }
+	//Setters
+	//Might not actually use these but good to have anyway -XQ
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    //Setters
-    //Might not actually use these but good to have anyway -XQ
-    public void setTitle(String title) {
-        this.title = title
-    }
+ 	public void setAuthor(String author) {
+ 		this.author = author;
+ 	}
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+ 	public void setGenre(String genre) {
+ 		this.genre = genre;
+ 	}
 
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
+ 	public void setAverageRating(double averageRating) {
+ 		this.averageRating = averageRating;
+ 	}
+  
+ 	public void setReviewList(ReviewList reviewList) {
+ 		this.reviewList = reviewList;
+ 	}
+  
+ 	//Update average rating method - updates the average rating of the book based on its reviews
+ 	public void updateAverageRating() {
+ 		averageRating = reviewList.calculateAverageRating();
+ 	}
 
-    public void setAverageRating(double averageRating) {
-        this.averageRating = averageRating;
-    }
-
-    //toString method override for testing purposes
-    @Override
-    public String toString() {
-        return "BOOK - Title: " + title + ", Author: " + author + ", Genre: " + genre +
-            ", Avg. Rating: " + rating;
-    }
+	
+ 	//toString method override for testing purposes
+ 	@Override
+ 	public String toString() {
+ 		return "\nBOOK - Title: " + title + ", Author: " + author + ", Genre: " + genre +
+ 				", Avg. Rating: " + averageRating + "\n";
+ 	}
 }
